2a35f12eb21894d7ed2beb2b4f3e99459f4db414
Andrea Pasqualini <andreapasqualini@outlook.com>
2017-12-15 12:43:54 +0100

    Revert "Used LaTeX in text rendering in the graph"
    This reverts commit 870dbff4334dd38e7c881b50c6cfbb37579afd58.

--- tests/t_spectrum.py
+++ tests/t_spectrum.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy.random import normal as randn
 import tspy as ts
 import matplotlib.pyplot as plt
-plt.rc('text', usetex=True)
+plt.rc('text', usetex=False)
 
 
 def gen_ar(T, coeffs):
